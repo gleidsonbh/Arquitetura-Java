58199b62171ed874eca4b586df33c22aeff05524
Gleidson Souza <gleidson.guilherme@msn.com>
2022-10-31 22:56:34 -0300

    Feature 01 - Criando demais classes

---
+++ appmedicamento/src/main/java/br/edu/infnet/appmedicamento/HigienePessoalTeste.java
@@ -0,0 +1,5 @@
+package br.edu.infnet.appmedicamento;
+
+public class HigienePessoalTeste {
+
+}
--- appmedicamento/src/main/java/br/edu/infnet/appmedicamento/model/domain/HigienePessoal.java
+++ appmedicamento/src/main/java/br/edu/infnet/appmedicamento/model/domain/HigienePessoal.java
@@ -2,4 +2,12 @@ package br.edu.infnet.appmedicamento.model.domain;
 
 public class HigienePessoal extends Produto {
 
+	public String precaucoes;
+	public String perfil;
+	public String genero;
+	
+	@Override
+	public String toString() {
+		return precaucoes + ";" + perfil + ";" + genero;
+	}	
 }
